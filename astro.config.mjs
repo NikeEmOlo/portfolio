7389879adc098d359799509452b38a01d4666ed3
Nike <198646766+BlakKMango@users.noreply.github.com>
2026-05-29 00:10:35 +0100

    Make Tarot cards accessible
    - Tarot cards are tab-able
    - Tarot cards hghlight when tabbed through

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    fonts: [{
+    fonts: [
+    {
         provider: fontProviders.fontsource(),
         name: "David Libre",
-        cssVariable: "--david"
+        cssVariable: "--main-font"
+    },
+    {
+        provider: fontProviders.fontsource(),
+        name: "DM Sans",
+        cssVariable: "--secondary-font"
     }]
 });
